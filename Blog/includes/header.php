293822e62b8461dba0f1721e8b520433a7a5007b
LucasBouet <l.bouet@ecole-ipssi.net>
2025-11-18 16:47:17 +0100

    created

---
+++ Blog/includes/header.php
@@ -0,0 +1,6 @@
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<ul>
+    <li>test1</li>
+    <li>test2</li>
+</ul>
+
---
+++ Blog/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My super webpage</title>
+    <link rel="stylesheet" href="./assets/css/tailwind.css">
+</head>
+<body>
+    <?php 
+        include_once("./includes/functions.php");
+        include_once("./data/articles.php");
+
+        include_once("./includes/header.php");
+
+
+
+        include_once("./includes/footer.php");
+    ?>
+</body>
+</html>
