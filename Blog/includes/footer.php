328936fe8de3dbe5bdd854f615744bf08b3685b2
LucasBouet <l.bouet@ecole-ipssi.net>
2025-11-19 11:52:11 +0100

    footer

--- Blog/includes/footer.php
+++ Blog/includes/footer.php
@@ -1,3 +1,3 @@
 <div class="bg-green-500">
-    <h1 class="text-2xl underline py-auto font-medium uppercase p-15 text-white">&copy all rights reserved</h1>
+    <h1 class="text-2xl underline py-auto font-medium uppercase p-15 text-white">&copy <?php echo date("Y") ?> all rights reserved</h1>
 </div>
--- Blog/includes/header.php
+++ Blog/includes/header.php
@@ -1,7 +1,7 @@
 <a href="index.php">
     <div class="bg-green-500 flex flex-row justify-center gap-1 py-5 items-center content-center">
         <img src="assets/svg/home.svg" alt="Logo" class=" w-10 h-10">
-        <h1 class="text-3xl underline text-center font-black uppercase py-15 text-white">Home</h1>
+        <h1 class="text-3xl underline text-center font-black uppercase py-15 text-white">Home!</h1>
     </div>
 </a>
 
