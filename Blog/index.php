b20a9f69d63eb86728ba5ce565b982a0eb508e2c
LucasBouet <l.bouet@ecole-ipssi.net>
2025-11-19 08:51:38 +0100

    files merging

--- Blog/index.php
+++ Blog/index.php
@@ -5,18 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mes super articles</title>
     <link rel="stylesheet" href="./assets/css/tailwind.css">
-    <style>
-        @keyframes slideInRight {
-            from {
-                opacity: 0;
-                transform: translateX(40px);
-            }
-            to {
-                opacity: 1;
-                transform: translateX(0);
-            }
-        }
-    </style>
 </head>
 <body>
     <?php 
