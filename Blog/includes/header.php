c3b16c648e8b594642e2160568f77110b2aa77d6
LucasBouet <l.bouet@ecole-ipssi.net>
2025-11-19 09:03:14 +0100

    svg addition

--- Blog/includes/header.php
+++ Blog/includes/header.php
@@ -1,6 +1,7 @@
-<div class="bg-green-500">
-    <a href="index.php">
-        <h1 class="text-3xl underline text-center font-black uppercase p-15 text-white">Home</h1>
-    </a>
-</div>
+<a href="index.php">
+    <div class="bg-green-500 flex flex-row justify-center gap-1 py-5 items-center content-center">
+        <img src="assets/svg/home.svg" alt="Logo" class=" w-10 h-10">
+        <h1 class="text-3xl underline text-center font-black uppercase py-15 text-white">Home</h1>
+    </div>
+</a>
 
